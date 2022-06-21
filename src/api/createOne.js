12c1051f71d22f7d0353c567c418d41196b5799c
JIYEONGSTAR <jiyeongstar@gmail.com>
2022-06-21 16:30:15 +0900

    react로 나만의 아고라 스테이츠 만들기

---
+++ src/api/createOne.js
@@ -0,0 +1,21 @@
+import React from "react";
+const URL = "http://localhost:3001/discussions";
+const createOne = (content) => {
+  console.log(content);
+  let body = {
+    author: content.name,
+    title: content.title,
+    createdAt: new Date().toISOString(),
+    avatarUrl: "https://avatars.githubusercontent.com/u/53474999?v=4",
+    bodyHTML: content.story,
+  };
+  fetch(URL, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+export default createOne;
---
+++ src/api/findAll.js
@@ -0,0 +1,11 @@
+import React from "react";
+const URL = "http://localhost:3001/discussions";
+const findAll = (currentPage) => {
+  const getFindAll = fetch(`${URL}?page=${currentPage}`)
+    .then((res) => res.json())
+    .catch((error) => console.log(error));
+
+  return getFindAll;
+};
+
+export default findAll;
